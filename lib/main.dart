2e67ebd4c950da3dcf62091116202b46ce8c3475
eodudrepublic <eodudrepublic@gmail.com>
2025-01-04 16:58:22 +0900

    feat - 카카오 소셜 로그인 기능 추가를 위한 안드로이드 등록 완료 - 키값은 숨김 - 이제 소셜 로그인 화면 만들어서 테스트 진행 예정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:battleship_fe/view/game/game_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'common/key.dart';
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  KakaoSdk.init(nativeAppKey: myNativeAppKey);
 
   runApp(MyApp());
 }
