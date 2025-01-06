6cb799db2ed30fa9dea5ddcece1fb76281aaf397
eodudrepublic <eodudrepublic@gmail.com>
2025-01-06 22:59:12 +0900

    feat - 서버에 맞게 id -> user_id(카카오톡 id), 자료형 정수형으로 수정 - 서버 수정사항 반영

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -2,7 +2,7 @@ import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import '../common/utils/logger.dart';
 
 class AppUser {
-  final String id;
+  final int id;
   final String? nickname;
   final String? profileImageUrl;
 
@@ -15,11 +15,11 @@ class AppUser {
   // Kakao SDK의 User 객체를 AppUser로 변환하는 팩토리 생성자
   factory AppUser.fromKakaoUser(User kakaoUser) {
     Log.info("Kakao Socail Login :\n"
-        "id : ${kakaoUser.id}\n"
+        "user_id : ${kakaoUser.id}\n"
         "nickname : ${kakaoUser.kakaoAccount?.profile?.nickname}\n"
         "profileImageUrl : ${kakaoUser.kakaoAccount?.profile?.profileImageUrl}");
     return AppUser(
-      id: kakaoUser.id.toString(),
+      id: kakaoUser.id,
       nickname: kakaoUser.kakaoAccount?.profile?.nickname,
       profileImageUrl: kakaoUser.kakaoAccount?.profile?.profileImageUrl,
     );
